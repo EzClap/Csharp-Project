5663e3286cd3c4b51a2cb8919141992e9308fa2b
jyxpe <jyxpe123@gmail.com>
2018-10-06 12:50:15 +1000

    addendum

--- Object Classes/Die.cs
+++ Object Classes/Die.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.IO;
 
 namespace Object_Classes {
 
-   
+
 
     /// <summary>
     /// Represents a dice with many face values that can be rolled.
@@ -15,6 +16,10 @@ namespace Object_Classes {
         private const int MIN_FACES = 4;
         private const int DEFAULT_FACE_VALUE = 1;
         private const int SIX_SIDED = 6;
+        private static string defaultPath = Environment.CurrentDirectory;
+        private static string rollFileName = defaultPath + "\\testrolls.txt";
+        private static StreamReader rollFile = new StreamReader(rollFileName);
+        private static bool DEBUG = true;
 
         // number of sides on the die
         private int numOfFaces;
@@ -67,9 +72,16 @@ namespace Object_Classes {
        //  Rolls the die and returns the result.
        //-----------------------------------------------------------------
        public int Roll() {
-           faceValue =  random.Next(NumOfFaces) + 1;
-           return FaceValue;
-       }
+            if (!DEBUG)
+            {
+                faceValue = random.Next(NumOfFaces) + 1;
+            }
+            else
+            {
+                faceValue = int.Parse(rollFile.ReadLine());
+            }
+            return FaceValue;
+        }
      
      
        //-----------------------------------------------------------------
